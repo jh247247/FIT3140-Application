113154542c61eead35581225a07e33f37bd3a579
Jack Hosemans <jh247247@gmail.com>
2014-03-24 17:06:54 +1100

    Fixed up some GUI oops in the xml

--- gen/com/jack/nowlayout/R.java
+++ gen/com/jack/nowlayout/R.java
@@ -29,16 +29,15 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_select=0x7f0a0009;
+        public static final int action_select=0x7f0a0008;
         public static final int action_settings=0x7f0a0000;
         public static final int button1=0x7f0a0004;
         public static final int card_action=0x7f0a0003;
         public static final int card_body=0x7f0a0002;
         public static final int card_heading=0x7f0a0001;
-        public static final int card_image=0x7f0a0006;
-        public static final int card_image_layout=0x7f0a0005;
-        public static final int imageView=0x7f0a0007;
-        public static final int mainLayout=0x7f0a0008;
+        public static final int card_image=0x7f0a0005;
+        public static final int imageView=0x7f0a0006;
+        public static final int mainLayout=0x7f0a0007;
     }
     public static final class integer {
         public static final int config_slide_time=0x7f060000;
