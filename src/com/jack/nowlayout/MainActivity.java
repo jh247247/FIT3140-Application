bbb5deb3948ac01da511139af11d7e24e47c27b8
tjpar4 <tjpar4@student.monash.edu>
2014-03-24 14:39:44 +1100

    Added image displaying functionality
    It's kind of janky for now, but the app will now set aside some room at
    the bottom of the page for image displaying and show the image that you
    select underneath the two test actions.

--- src/com/jack/nowlayout/MainActivity.java
+++ src/com/jack/nowlayout/MainActivity.java
@@ -2,6 +2,7 @@ package com.jack.nowlayout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.view.View;
 import android.view.LayoutInflater;
@@ -19,91 +20,96 @@ import android.graphics.BitmapFactory;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
-    /*This is where all the constants for this activity live.*/
-    private static final int ACTIVITY_SELECT_IMAGE=1;
-    /*This is all the changing data lives, ones that should be saved upon
+	/*This is where all the constants for this activity live.*/
+	private static final int ACTIVITY_SELECT_IMAGE=1;
+	/*This is all the changing data lives, ones that should be saved upon
       shutdown. I don't know whether or not there should be a database
       for this or  something.*/
-    private ArrayList<Bitmap> m_imageBuffer;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	private ArrayList<Bitmap> m_imageBuffer;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-	// This is testing stuff for template layout inflating.
-	// I have yet to decide the best way to implement this dynamically
-        Context context = getApplicationContext();
-        LinearLayout container = (LinearLayout) findViewById(R.id.mainLayout);
-        LayoutInflater inflater = (LayoutInflater) LayoutInflater.from(context);
-        View test = inflater.inflate(R.layout.card_action, null);
-        container.addView(test);
-        View test2 = inflater.inflate(R.layout.card_info, null);
-        container.addView(test2);
+		// This is testing stuff for template layout inflating.
+		// I have yet to decide the best way to implement this dynamically
+		Context context = getApplicationContext();
+		LinearLayout container = (LinearLayout) findViewById(R.id.mainLayout);
+		LayoutInflater inflater = (LayoutInflater) LayoutInflater.from(context);
+		View test = inflater.inflate(R.layout.card_action, null);
+		container.addView(test);
+		View test2 = inflater.inflate(R.layout.card_info, null);
+		container.addView(test2);
+		View testimage = inflater.inflate(R.layout.image, null);
+		container.addView(testimage);
 
-	// We should restore state at this point, I think.
-	// Have to look into how android talks to apps and how they do
-	// states and stuff.
-	m_imageBuffer = new ArrayList<Bitmap>();
-    }
+		// We should restore state at this point, I think.
+		// Have to look into how android talks to apps and how they do
+		// states and stuff.
+		m_imageBuffer = new ArrayList<Bitmap>();
+	}
 
-    // TODO: read the android guidelines for saving state etc.
+	// TODO: read the android guidelines for saving state etc.
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.actionbar_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.actionbar_main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-        switch (item.getItemId()) {
-        case R.id.action_settings:
-            Context ctx = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(ctx, "This is a test action!",
-                                         duration);
-            toast.show();
-            return true;
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			Context ctx = getApplicationContext();
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(ctx, "This is a test action!",
+					duration);
+			toast.show();
+			return true;
 
-        case R.id.action_select:
-            Intent i = new Intent(Intent.ACTION_PICK,
-                                  android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(i, ACTIVITY_SELECT_IMAGE);
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-
-        switch(requestCode) {
-        case ACTIVITY_SELECT_IMAGE:
-            if(resultCode == RESULT_OK){
-		Uri selectedImage = imageReturnedIntent.getData();
-		Context ctx = getApplicationContext();
-		int duration = Toast.LENGTH_SHORT;
-		Toast toast = Toast.makeText(ctx, selectedImage.getPath(),
-					     duration);
-		toast.show();
-		// NOTE: at this stage, image can be downscaled on the fly.
-		// might not be a good idea to do this as we would be
-		// bandwidth limited...
-		InputStream imageStream = null;
-		try {
-		    imageStream = getContentResolver().openInputStream(selectedImage);
+		case R.id.action_select:
+			Intent i = new Intent(Intent.ACTION_PICK,
+					android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			startActivityForResult(i, ACTIVITY_SELECT_IMAGE);
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-		catch (Exception e) {
-		    // What should I do here?!
-		    return;
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+		super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+		switch(requestCode) {
+		case ACTIVITY_SELECT_IMAGE:
+			if(resultCode == RESULT_OK){
+				Uri selectedImage = imageReturnedIntent.getData();
+				Context ctx = getApplicationContext();
+				int duration = Toast.LENGTH_SHORT;
+				Toast toast = Toast.makeText(ctx, selectedImage.getPath(),
+						duration);
+				toast.show();
+				// NOTE: at this stage, image can be downscaled on the fly.
+				// might not be a good idea to do this as we would be
+				// bandwidth limited...
+				InputStream imageStream = null;
+				try {
+					imageStream = getContentResolver().openInputStream(selectedImage);
+				}
+				catch (Exception e) {
+					// What should I do here?!
+					return;
+				}
+				m_imageBuffer.add(BitmapFactory.decodeStream(imageStream));
+				//Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
+				ImageView image = (ImageView) findViewById(R.id.imageView);
+				image.setImageURI(selectedImage);
+			}
 		}
-                m_imageBuffer.add(BitmapFactory.decodeStream(imageStream));
-            }
-        }
-    }
+	}
 
 }
