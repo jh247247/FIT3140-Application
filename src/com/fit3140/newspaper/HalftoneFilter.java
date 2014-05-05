dbeee5830709b960a14fc6ec499d85db3f1fddc6
Jack Hosemans <jh247247@gmail.com>
2014-05-05 16:25:30 +1000

    Finally made the fragment do something with the UI. Should start doing processing stuff soon.

--- src/com/fit3140/newspaper/HalftoneFilter.java
+++ src/com/fit3140/newspaper/HalftoneFilter.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 /**
  * This class applies a halftoning filter to the passed in
@@ -16,10 +19,15 @@ import android.view.ViewGroup;
  *
  */
 
-class HalftoneFilter extends Filter{
+class HalftoneFilter extends Filter implements OnSeekBarChangeListener
+{
   // This is a setting for basic halftoning, other implementations
   // come later.
-  protected int m_gridsize;
+  private int m_gridSize;
+  private SeekBar m_gridSizeBar;
+  private TextView m_gridSizeText;
+
+
   // These are just here because they were easy to write.
   // Should give ideas on how to implement the more advance halftoning
   // later on. Uncomment so that they are actually defined when we
@@ -31,18 +39,50 @@ class HalftoneFilter extends Filter{
   public View onCreateView(LayoutInflater inflater,
 			   ViewGroup container,
 			   Bundle savedInstanceState) {
+
+    View ret = inflater.inflate(R.layout.fragment_filter_halftone,
+				container,
+				false);
+    m_gridSizeText = (TextView)ret.findViewById(R.id.grid_size_text);
+    m_gridSizeBar = (SeekBar)ret.findViewById(R.id.grid_size_seekbar);
+
+    //Fixme
+    m_gridSizeText.setText("0");
+    m_gridSizeBar.setOnSeekBarChangeListener(this);
+
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_filter_halftone,
-			    container,
-			    false);
+    return ret;
   }
+
+
   /**
    * This method should set the grid size for the halftoning when the
    * user changes it in the displayed fragment.
    *
    */
   protected void setGridSize(int gridsize) {
-    // TODO
+    m_gridSize = gridsize;
+    m_gridSizeText.setText(String.valueOf(gridsize));
+  }
+
+  /**
+   * This gets called whenever any child seekbar gets changed.
+   *
+   */
+  @Override
+    public void onProgressChanged(SeekBar seekBar,
+				  int progress,
+				  boolean fromUser) {
+    // currently placeholder. Can be changed.
+    setGridSize(progress);
+  }
+
+  public void onStartTrackingTouch(SeekBar seekBar) {
+
+  }
+
+  public void onStopTrackingTouch(SeekBar seekBar) {
+
   }
 
   /**
